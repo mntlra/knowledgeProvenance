524eeedd047862aa030cbdb8e8ed92bb76890ea0
mntlra <laura.menotti98@gmail.com>
2023-12-14 17:38:23 +0100

    bug fixes

--- py/serialize_nanopublications.py
+++ py/serialize_nanopublications.py
@@ -3,11 +3,11 @@ from Logger import DebugLogger
 from CoreNanopub.CoreNanopub import CoreNanopub
 
 # Logger
-Logger = DebugLogger("sample")
+Logger = DebugLogger("full")
 
 Logger.logger.info(f'-----\nCORE NANOPUB SERIALIZATION (run: {datetime.now()})\n-----')
 
 # Nanopubs creation and serialization
-npg = CoreNanopub().create_nanopub_graphs(sample=True)
+npg = CoreNanopub().create_nanopub_graphs(sample=False)
 
 Logger.logger.info(f'-----\nCORE NANOPUB SERIALIZATION COMPLETED at {datetime.now()}\n-----')
